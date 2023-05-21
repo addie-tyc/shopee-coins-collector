fc0f09680a73c86c1fd4bb5eb1e14e04d6dabc2b
addie-tyc <addiechung.tyc@gmail.com>
2023-05-22 02:03:29 +0800

    doc: use env in logger level

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,12 +1,14 @@
 import * as winston from 'winston';
 
+const { LOGGER_LEVEL } = process.env;
+
 export const logger = winston.createLogger({
   level: 'debug',
   exitOnError: true,
   format: winston.format.json(),
   transports: [
     new winston.transports.Console({
-      level: 'info',
+      level: LOGGER_LEVEL || 'info',
       stderrLevels: ['debug', 'info', 'warn', 'error'],
       format: winston.format.combine(
         winston.format.colorize(),
