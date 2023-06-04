32246f911a4055b6ebace707ad0ae86afff29fa2
addie-tyc <addiechung.tyc@gmail.com>
2023-06-05 00:57:48 +0800

    feat: evaluate script on every page

--- lib/crawlers/BaseCrawler.ts
+++ lib/crawlers/BaseCrawler.ts
@@ -6,6 +6,7 @@ import { wait } from '../util';
 export interface ICrawlerOptions {
   parallelism?: number
   cacheExpirySeconds?: number
+  args?: String[]
 }
 
 interface ICrawlerFullOptions extends ICrawlerOptions {
@@ -71,17 +72,21 @@ export abstract class BaseCrawler {
 
   async newPage (url: string|undefined): Promise<Page> {
     const browser = await this.getBrowser(this.launchOptions);
+    browser.on('targetcreated', async (target) => {
+      if (target.type() === 'page') {
+        const page = await target.page();
+        await page.evaluateOnNewDocument(() => {
+          // solution from https://github.com/wdzeng/shopee-coins-bot/issues/53 not working
+          Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined
+          });
 
-    const page = await browser.newPage();
-    await page.evaluateOnNewDocument(() => {
-      // solution from https://github.com/wdzeng/shopee-coins-bot/issues/53 not working
-      // Object.defineProperty(navigator, 'webdriver', {
-      //   get: () => undefined
-      // });
-
-      // use the workaround from https://stackoverflow.com/questions/56335066/changing-window-navigator-within-puppeteer-to-bypass-antibot-system
-      delete navigator.__proto__.webdriver;
+          // another the workaround from https://stackoverflow.com/questions/56335066/changing-window-navigator-within-puppeteer-to-bypass-antibot-system
+          // delete navigator.__proto__.webdriver;
+        });
+      }
     });
+    const page = await browser.newPage();
     if (url) {
       await page.goto(url);
     }
